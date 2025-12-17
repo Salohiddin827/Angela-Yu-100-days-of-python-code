7175be5ad2df4d372dc277fc8036080fa59e3fad
Salohiddin827 <salohiddinbekmirzayev827@gmail.com>
2025-12-17 20:23:27 +0900

    Day 7

--- Day 7/Hangman_Step1.py
+++ Day 7/Hangman_Step1.py
@@ -1,13 +1,30 @@
 import random
 words = ["Salohiddin","jamie","Jacky","John","Arthur"]
 random_word = random.choice(words).lower()
-# for letter in random_word:
-#     letter.replace(letter,"_")
-
+placeholder = ""
 print(random_word)
-guess = input("Guess a letter")
-for l in random_word:
-    if guess ==l:
-        print("Right")
-    else:
-        print("wrong")
+placeholder = ""
+for position in range(1,len(random_word)+1):
+    placeholder+="_"
+print(placeholder)
+Game_over  = False
+correct_list = []
+while not Game_over:
+    guess = input("Guess a letter \n") 
+    display = ""
+    for letter in random_word:
+        if guess ==letter:
+            display+=letter
+            correct_list.append(letter)
+        elif letter in correct_list:
+            display+=letter
+
+        else:
+            display+="_"
+
+
+    print(display)
+
+    if "_" not in display:
+        Game_over = True
+        print("You win")
