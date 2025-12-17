580d5297dd827e46f4b83790cda4d763eafa191f
Salohiddin827 <salohiddinbekmirzayev827@gmail.com>
2025-12-17 21:41:38 +0900

    Day 7

--- Day 7/Hangman_Step1.py
+++ Day 7/Hangman_Step1.py
@@ -1,7 +1,65 @@
 import random
-words = ["Salohiddin","jamie","Jacky","John","Arthur"]
-random_word = random.choice(words).lower()
+import hangman
+stages = [r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+random_word = random.choice(hangman.word_list).lower()
 placeholder = ""
+lives = 5
 print(random_word)
 placeholder = ""
 for position in range(1,len(random_word)+1):
@@ -10,8 +68,10 @@ print(placeholder)
 Game_over  = False
 correct_list = []
 while not Game_over:
+    print(f"You have total {lives} lives")
     guess = input("Guess a letter \n") 
     display = ""
+
     for letter in random_word:
         if guess ==letter:
             display+=letter
@@ -24,7 +84,22 @@ while not Game_over:
 
 
     print(display)
+    if guess not in random_word:
+        print(f"You have already guessed {guess}")
+        lives-=0
 
     if "_" not in display:
         Game_over = True
         print("You win")
+    if guess not in random_word:
+        lives-=1
+        print(f"Now You have {lives} lives left")
+
+        if lives ==0:
+            print(f"You lost!\n The correct word was {random_word}" 
+            )
+            Game_over = True
+        elif lives ==1:
+            print(f"Now You have {lives} live left")
+
+    print(stages[lives])
---
+++ Day 7/hangman.py
@@ -0,0 +1,11 @@
+word_list = [
+    "apple", "banana", "orange", "grape", "melon",
+    "computer", "keyboard", "mouse", "screen", "laptop",
+    "python", "java", "coding", "program", "developer",
+    "hangman", "project", "function", "variable", "loop",
+    "school", "student", "teacher", "university", "lesson",
+    "music", "guitar", "piano", "violin", "drums",
+    "football", "basketball", "tennis", "cricket", "hockey",
+    "travel", "country", "city", "village", "airport",
+    "friend", "family", "people", "child", "parent"
+]
